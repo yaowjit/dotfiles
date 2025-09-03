395a89568215e8431dedc071330711ef36a54cc7
Yao Weijie <yaowj.it@outlook.com>
2025-09-04 00:42:18 +0800

    add esquelote plugin cmd alias

--- config/nvim/lua/plugins/esqueleto.lua
+++ config/nvim/lua/plugins/esqueleto.lua
@@ -10,4 +10,9 @@ return {
             return vim.fn.readdir(dir)
         end,
     },
+    config = function(_, opts)
+        require("esqueleto").setup(opts)
+        vim.cmd("command TemplateInsert EsqueletoInsert") -- create cmd alias
+        vim.cmd("command TemplateNew EsqueletoNew")
+    end,
 }
