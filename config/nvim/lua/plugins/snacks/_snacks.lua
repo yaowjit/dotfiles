ab87ac313c12b4a3c0dc1c058143429f2abb3577
Yao Weijie <yaowj.it@outlook.com>
2025-09-04 23:46:10 +0800

    update

--- config/nvim/lua/plugins/snacks/_snacks.lua
+++ config/nvim/lua/plugins/snacks/_snacks.lua
@@ -1,11 +1,3 @@
-_G.inspect = function(...)
-    Snacks.debug.inspect(...)
-end
-vim.print = _G.inspect
-_G.log = function(...)
-    Snacks.debug.log(...)
-end
-
 ---@type LazySpec
 return {
     "folke/snacks.nvim",
@@ -87,4 +79,24 @@ return {
         { "<leader>q", function() Snacks.bufdelete() end, desc = "buf delete" },
         -- stylua: ignore end
     },
+    init = function()
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "VeryLazy",
+            callback = function()
+                vim.print = Snacks.debug.inspect
+                _G.log = Snacks.debug.log
+
+                local cmdparse = require("mega.cmdparse")
+                local picker_parser = cmdparse.ParameterParser.new({ name = "Snacks", help = "" })
+                local top_subparsers = picker_parser:add_subparsers({ destination = "commands" })
+                for picker, picker_func in pairs(Snacks.picker) do
+                    local subparser = top_subparsers:add_parser({ name = picker, help = "" })
+                    subparser:set_execute(function(data)
+                        picker_func()
+                    end)
+                end
+                cmdparse.create_user_command(picker_parser)
+            end,
+        })
+    end,
 }
--- config/nvim/lua/plugins/snacks/picker.lua
+++ config/nvim/lua/plugins/snacks/picker.lua
@@ -1,29 +1,3 @@
-local pickers_config = {
-    lsp_symbols = {
-        filter = {
-            default = { "Class", "Constructor", "Function", "Method" },
-        },
-        workspace = false,
-    },
-}
-
-vim.api.nvim_create_user_command("Snacks", function(args)
-    local snacks_pickers = vim.tbl_keys(Snacks.picker)
-    local picker = args.args:match("[%w_]+")
-
-    if vim.tbl_contains(snacks_pickers, picker, {}) then
-        Snacks.picker[picker](pickers_config[picker] or {})
-    else
-        vim.notify("unsupported Snacks picker")
-    end
-end, {
-    nargs = 1,
-    complete = function(argLead, cmdline, cursorPos)
-        local args = vim.split(cmdline, " ", { trimempty = true })
-        return (#args == 1) and vim.tbl_keys(Snacks.picker) or {}
-    end,
-})
-
 ---@type LazySpec
 return {
     "folke/snacks.nvim",
@@ -63,7 +37,7 @@ return {
             { "<leader>fD", function() Snacks.picker.lsp_type_definitions() end, desc = "lsp_type_definitions" },
             { "<leader>fr", function() Snacks.picker.lsp_references() end, desc = "lsp_references" },
             { "<leader>fi", function() Snacks.picker.lsp_implementations() end, desc = "lsp_implementations" },
-            { "<leader>fs", function() Snacks.picker.lsp_symbols(pickers_config.lsp_symbols) end, desc = "lsp_symbols" },
+            { "<leader>fs", function() Snacks.picker.lsp_symbols() end, desc = "lsp_symbols" },
             { "<leader>fS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "lsp_workspace_symbols" },
             -- stylua: ignore end
         })
