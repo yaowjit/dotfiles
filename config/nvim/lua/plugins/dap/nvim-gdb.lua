2bc92307be5b6d99fe5e3ff3aaaa343b45357762
Yao Weijie <yaowj.it@outlook.com>
2025-10-04 22:23:31 +0800

    fix c-e

--- config/nvim/lua/plugins/dap/nvim-gdb.lua
+++ config/nvim/lua/plugins/dap/nvim-gdb.lua
@@ -4,5 +4,6 @@ return {
     "sakhnik/nvim-gdb",
     config = function()
         vim.g.nvimgdb_disable_start_keymaps = true
+        vim.keymap.set("c", "<C-e>", "<End>", { noremap = true }) -- override
     end,
 }
