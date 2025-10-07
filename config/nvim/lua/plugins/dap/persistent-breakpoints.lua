d3da8cf351de759a5afacba000759dcec44ef69b
Yao Weijie <yaowj.it@outlook.com>
2025-10-08 04:10:33 +0800

    fix

--- config/nvim/lua/plugins/dap/persistent-breakpoints.lua
+++ config/nvim/lua/plugins/dap/persistent-breakpoints.lua
@@ -6,18 +6,17 @@ return {
         always_reload = true,
     },
     lazy = false,
-    config = function(_, otps)
+    config = function(_, opts)
         require("persistent-breakpoints").setup(opts)
         -- dap configured filetypes
         vim.api.nvim_create_autocmd("FileType", {
             pattern = { "python", "c", "cpp" },
             callback = function(event)
-                local opt = { buffer = event.buf }
-                -- vim.keymap.set("n", "<leader>b", require("persistent-breakpoints.api").toggle_breakpoint, opt)
-                vim.keymap.set("n", "<2-LeftMouse>", require("persistent-breakpoints.api").toggle_breakpoint, opt)
-                vim.api.nvim_buf_create_user_command(event.buf, "DapRunToCursor", function()
-                    require("dap").run_to_cursor()
-                end, {})
+                local keymap_opt = { buffer = event.buf }
+                -- stylua: ignore start
+                vim.keymap.set("n", "<2-LeftMouse>", function() require("persistent-breakpoints.api").toggle_breakpoint() end, keymap_opt)
+                vim.api.nvim_buf_create_user_command(event.buf, "DapRunToCursor", function() require("dap").run_to_cursor() end, {})
+                -- stylua: ignore end
             end,
         })
     end,
