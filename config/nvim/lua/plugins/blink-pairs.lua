dec4cbf34107784e65ce756bc79fc06145c0f173
Yao Weijie <yaowj.it@outlook.com>
2026-01-16 00:32:51 +0800

    use blink pairs

---
+++ config/nvim/lua/plugins/blink-pairs.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+    "saghen/blink.pairs",
+    dependencies = "saghen/blink.download",
+    version = "*",
+    opts = {
+        highlights = {
+            groups = {
+                "BlinkPairsBlue",
+                "BlinkPairsPurple",
+                "BlinkPairsGreen",
+                "BlinkPairsCyan",
+                "BlinkPairsOrange",
+                "BlinkPairsYellow",
+            },
+        },
+    },
+}
